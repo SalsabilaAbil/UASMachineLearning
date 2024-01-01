84eb30576355fb53f547c787dadb93edea2be9f2
SalsabilaAbil <149142675+SalsabilaAbil@users.noreply.github.com>
2024-01-01 20:12:31 +0700

    Update Bakery.py

--- Bakery.py
+++ Bakery.py
@@ -25,7 +25,7 @@ def get_data( month ='' , day = ''):
     return filtered if filtered.shape[0] else "No Result!"
 
 def user_input_features():
-    item = st.selectbox("Item", ['whole milk','other vegetables','rolls/buns','soda','yogurt','root vegetables','tropical fruit','bottled water','sausage','citrus fruit','pastry','pip fruit','shopping bags','canned beer','bottled beer','whipped/sour cream','newspapers','frankfurter','brown bread','pork','domestic eggs','butter','fruit/vegetable juice','beef','curd'])
+    item = st.selectbox("Item", ['Bread','whole milk','other vegetables','rolls/buns','soda','yogurt','root vegetables','tropical fruit','bottled water','sausage','citrus fruit','pastry','pip fruit','shopping bags','canned beer','bottled beer','whipped/sour cream','newspapers','frankfurter','brown bread','pork','domestic eggs','butter','fruit/vegetable juice','beef','curd'])
     month = st.select_slider("Month", ["Jan","Feb","Mar","Apr","Mei","Jun","Jul","Agu","Sep","Okt","Nov","Des"])
     day = st.select_slider("Day", ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"], value='Senin')
 
